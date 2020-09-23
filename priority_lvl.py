6dd4de7a6d96e6c4273581e043b22a8e8252e8be
DANCELME Loic <DANCEL@clb.loc>
2020-09-23 16:19:01 +0200

    update priority_lvl.py

--- priority_lvl.py
+++ priority_lvl.py
@@ -9,34 +9,46 @@ Created on Tue Sep  1 11:09:20 2020
 import pandas as pd
 import numpy as np
 import timeit
+from tqdm import tqdm 
 
 # //// Timer, file exec
 start = timeit.default_timer()
 # -----------------------------------------------------------------------------
 # //// Load file
-data = pd.read_csv('mean_std_v2.csv', dtype={'Taille' : float, 'Appli_origine' : str, 'mean' : float, 'std' : float, 'sem' : float}, na_values = '')
+data = pd.read_csv('data/age_interval/all_data.csv', dtype={'Taille' : float, 'Appli_origine' : str, 'mean' : float, 'std' : float, 'sem' : float}, na_values = '')
 
+applis = data.Appli.unique()
 
-priority_lvl3 = data[data['Appli'] != 'EVO']
-priority_lvl3 = priority_lvl3[priority_lvl3['Appli'] != 'VEN']
-priority_lvl3 = priority_lvl3[priority_lvl3['Appli'] != 'ORD']
-priority_lvl3 = priority_lvl3[priority_lvl3['Appli'] != 'XAL']
-priority_lvl2 = priority_lvl3[priority_lvl3['Appli'] != 'CRS']
-priority_lvl1 = priority_lvl2[priority_lvl2['Appli'] != 'CRC']
+lvl1 = ['DOS', 'BLO']
+lvl2 = ['DOS', 'BLO', 'CRC']
+lvl3 = ['DOS', 'BLO', 'CRC', 'CRS']
+lvl4 = applis
 
-print("priority_lvl4 : {}, taille : {}".format(data['Appli'].unique(), len(data)))
-print("priority_lvl3 : {}, taille : {}".format(priority_lvl3['Appli'].unique(), len(priority_lvl3)))
-print("priority_lvl2 : {}, taille : {}".format(priority_lvl2['Appli'].unique(), len(priority_lvl2)))
-print("priority_lvl1 : {}, taille : {}".format(priority_lvl1['Appli'].unique(), len(priority_lvl1)))
+zeros = 0* len(data)
+data["priority_lvl"] = zeros
 
+def test_lvl(i): 
+    x = 0
+    if i in lvl1:
+        x = 1
+    elif i in lvl2:
+        x = 2
+    elif i in lvl3:
+        x = 3
+    elif i in lvl4:
+        x = 4
+    return x
+           
+for idx, val in tqdm(enumerate(data.values)):
+    x = test_lvl(val[5])
+    data.iloc[idx,12] = x
+    print('Appli : {}\t priority_lvl:{}'.format(data.iloc[idx, 5], data.iloc[idx, 12]))
 
-# priority_lvl1.to_csv('priority_lvl1.csv', index=False)
-# priority_lvl2.to_csv('priority_lvl2.csv', index=False)
-# priority_lvl3.to_csv('priority_lvl3.csv', index=False)
-
-
+# data.to_csv('data/age_interval/all_data.csv', index=False)
 
 # -----------------------------------------------------------------------------
+# Temps d'exec pour 447754 lignes = ~50 min
+#
 stop = timeit.default_timer()
 print()
 print('Time:  ', str(round(stop - start, 4)), 's\n\t' + str(round((stop - start) / 60, 4)) + ' m ')
