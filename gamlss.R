e765700b5d3a3341105e34dfef9694fa2a57875e
DANCELME Loic <DANCEL@clb.loc>
2020-11-27 11:06:29 +0100

    last commit

--- gamlss.R
+++
@@ -1,9 +0,0 @@
-# set current working directory
-setwd(dir="C:/Users/LOX/Desktop/CLB/test/Taille")
-
-# import libraries 
-library(gamlss)
-library(readr)
-
-# import data
-all_data <- read_csv("data/age_interval/all_data.csv")
